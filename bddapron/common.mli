4921d932f7f43291ad707febf07bbc7312294e6b
bjeannet <bjeannet@94497fdf-4533-0410-84ab-907f7b2fba56>
2011-12-30 23:05:38 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/bjeannet/pkg/bddapron/trunk@898 94497fdf-4533-0410-84ab-907f7b2fba56

---
+++ bddapron/common.mli
@@ -0,0 +1,34 @@
+(** Functions common to the two implementations of Combined Boolean/Numerical domain *)
+
+(* This file is part of the BDDAPRON Library, released under LGPL license.
+   Please read the COPYING file packaged in the distribution  *)
+
+val tcons0_array_of_cubecond :
+  (('a, 'b, 'c, 'd) Env.O.t as 'e) ->
+  ('a,'e) Cond.O.t ->
+  Cudd.Bdd.vt -> Apron.Tcons0.t array
+    (** Converts a cube of conditions into an array of APRON constraints *)
+
+val cube_split :
+  ('a, 'b) Cond.O.t -> Cudd.Bdd.vt -> Cudd.Bdd.vt * Cudd.Bdd.vt
+    (** Split a cube into a cube of Booleans and a cube of conditions *)
+val lvar_split :
+    ('a,'c,'d,'e) Env.O.t -> 'a list -> Cudd.Man.v Cudd.Bdd.t * Apron.Dim.t array
+    (** Split the list of variable into a positive cube (support)
+    of Boolean variables and an array of APRON dimensions *)
+
+val condition_of_tcons0 :
+  ('a, 'b, 'c, 'd) Env.O.t ->
+  Apron.Tcons0.t -> [ `Bool of bool | `Cond of 'a Apronexpr.Condition.t ]
+val bdd_of_condition :
+  (('a, 'b, 'c, 'd) Env.O.t as 'e) ->
+  ('a,'e) Cond.O.t ->
+  [< `Bool of bool | `Cond of 'a Apronexpr.Condition.t] -> Cudd.Bdd.vt
+val bdd_of_tcons0 :
+  (('a, 'b, 'c, 'd) Env.O.t as 'e) ->
+  ('a,'e) Cond.O.t ->
+  Apron.Tcons0.t -> Cudd.Bdd.vt
+val bdd_of_tcons0_array :
+  (('a, 'b, 'c, 'd) Env.O.t as 'e) ->
+  ('a,'e) Cond.O.t ->
+  Apron.Tcons0.t array -> Cudd.Bdd.vt
